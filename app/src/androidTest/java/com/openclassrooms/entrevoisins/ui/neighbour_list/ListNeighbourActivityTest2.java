ffea0e0faf80ae31edcd3283b10cfbeb53e6b4bc
mimi012 <jeremy.dufeux@gmail.com>
2020-11-16 19:38:02 +0100

    Add few tests

--- app/src/androidTest/java/com/openclassrooms/entrevoisins/ui/neighbour_list/ListNeighbourActivityTest2.java
+++ app/src/androidTest/java/com/openclassrooms/entrevoisins/ui/neighbour_list/ListNeighbourActivityTest2.java
@@ -1,24 +1,19 @@
+package com.openclassrooms.entrevoisins.ui.neighbour_list;
 
-package com.openclassrooms.entrevoisins.neighbour_list;
 
 import android.support.test.espresso.ViewInteraction;
-import android.support.test.espresso.contrib.RecyclerViewActions;
-import android.support.test.espresso.matcher.ViewMatchers;
+import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
 
 import com.openclassrooms.entrevoisins.R;
-import com.openclassrooms.entrevoisins.ui.neighbour_list.ListNeighbourActivity;
-import com.openclassrooms.entrevoisins.utils.DeleteViewAction;
 
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -26,80 +21,26 @@ import org.junit.runner.RunWith;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.swipeLeft;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
-import static android.support.test.espresso.matcher.ViewMatchers.assertThat;
-import static android.support.test.espresso.matcher.ViewMatchers.hasMinimumChildCount;
-import static android.support.test.espresso.matcher.ViewMatchers.isClickable;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withParent;
-import static com.openclassrooms.entrevoisins.utils.RecyclerViewItemCountAssertion.withItemCount;
 import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.core.IsNull.notNullValue;
 
-
-
-/**
- * Test class for list of neighbours
- */
+@LargeTest
 @RunWith(AndroidJUnit4.class)
-public class NeighboursListTest {
-
-    // This is fixed
-    private static int ITEMS_COUNT = 12;
-
-    private ListNeighbourActivity mActivity;
+public class ListNeighbourActivityTest2 {
 
     @Rule
-    public ActivityTestRule<ListNeighbourActivity> mActivityRule =
-            new ActivityTestRule(ListNeighbourActivity.class);
+    public ActivityTestRule<ListNeighbourActivity> mActivityTestRule = new ActivityTestRule<>(ListNeighbourActivity.class);
 
-    @Before
-    public void setUp() {
-        mActivity = mActivityRule.getActivity();
-        assertThat(mActivity, notNullValue());
-    }
-
-    /**
-     * We ensure that our recyclerview is displaying at least on item
-     */
     @Test
-    public void myNeighboursList_shouldNotBeEmpty() {
-        // First scroll to the position that needs to be matched and click on it.
-        onView(ViewMatchers.withId(R.id.list_neighbours))
-                .check(matches(hasMinimumChildCount(1)));
-    }
-
-    /**
-     * When we delete an item, the item is no more shown
-     */
-    @Test
-    public void myNeighboursList_deleteAction_shouldRemoveItem() {
-        // Given : We remove the element at position 2
-        onView(ViewMatchers.withId(R.id.list_neighbours)).check(withItemCount(ITEMS_COUNT));
-        // When perform a click on a delete icon
-        onView(ViewMatchers.withId(R.id.list_neighbours))
-                .perform(RecyclerViewActions.actionOnItemAtPosition(1, new DeleteViewAction()));
-        // Then : the number of element is 11
-        onView(ViewMatchers.withId(R.id.list_neighbours)).check(withItemCount(ITEMS_COUNT-1));
-    }
-
-    /**
-     * When we add a neighbour to favorites it is delayed in favorites tab
-     */
-    @Test
-    public void myFavoriteNeighbourList_addNeighbour_shouldBeOne(){
-        // Check if favorite list is empty
-        onView(ViewMatchers.withId(R.id.favorites_rv)).check(withItemCount(0));
-        // perform a click on a neighbour and add to favorites
+    public void listNeighbourActivityTest2() {
         ViewInteraction recyclerView = onView(
                 allOf(withId(R.id.list_neighbours),
                         withParent(withId(R.id.container))));
-        
         recyclerView.perform(actionOnItemAtPosition(0, click()));
-
 
         ViewInteraction floatingActionButton = onView(
                 allOf(withId(R.id.neighbour_detail_add_to_favorites), withContentDescription("Add to favorites"),
@@ -142,10 +83,6 @@ public class NeighboursListTest {
                                 1),
                         isDisplayed()));
         viewPager.perform(swipeLeft());
-        // Check if favorite list contains 1 item
-        onView(ViewMatchers.withId(R.id.favorites_rv)).check(withItemCount(1));
-
-
     }
 
     private static Matcher<View> childAtPosition(
